38fe491516dd04f076211e0304fbc5f7a2616d4f
Simon Tapson <sitapson@gmail.com>
2025-03-18 08:58:19 +0000

    Initial Commit

--- Package.swift
+++ Package.swift
@@ -1,21 +1,21 @@
-// swift-tools-version: 6.0
+// swift-tools-version: 5.10
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "CorvidaeLib",
+    platforms: [
+        .iOS(.v16) // iOS version target
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "CorvidaeLib",
-            targets: ["CorvidaeLib"]),
+            targets: ["CorvidaeCore"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "CorvidaeLib"),
-
+        // The destination of the framework you will obfuscate the code
+        .binaryTarget(name: "CorvidaeCore", path: "./Sources/CorvidaeCore.xcframework")
     ]
 )
---
+++ Sources/CorvidaeCore.xcframework/ios-arm64_x86_64-simulator/CorvidaeCore.framework/Headers/CorvidaeCore.h
@@ -0,0 +1,18 @@
+//
+//  CorvidaeCore.h
+//  CorvidaeCore
+//
+//  Created by Simon Tapson on 18/03/2025.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CorvidaeCore.
+FOUNDATION_EXPORT double CorvidaeCoreVersionNumber;
+
+//! Project version string for CorvidaeCore.
+FOUNDATION_EXPORT const unsigned char CorvidaeCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CorvidaeCore/PublicHeader.h>
+
+
